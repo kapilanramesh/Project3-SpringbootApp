4c8a90b4705d96f2b241ec60600710d2c073753f
kapilanramesh <kapilanramesh1@gmail.com>
2025-07-17 16:56:56 +0000

    My initial WAR App commit

---
+++ src/main/java/com/example/SpringbootWarAppApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWarAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWarAppApplication.class, args);
+    }
+}
